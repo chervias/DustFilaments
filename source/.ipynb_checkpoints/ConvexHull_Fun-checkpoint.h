d72f5c1a136d9628f7a6ac236f8c95c3bc581a6c
chervias <aconcagua.chc@gmail.com>
2021-07-05 18:41:57 -0400

    .ipynb_checkpoints removed

--- source/.ipynb_checkpoints/ConvexHull_Fun-checkpoint.h
+++
@@ -1,7 +0,0 @@
-bool ccw(const Point *a, const Point *b, const Point *c) ;
-int comparePoints(const void *lhs, const void *rhs) ;
-void fatal(const char* message) ;
-void* xmalloc(size_t n) ;
-void* xrealloc(void* p, size_t n) ;
-void printPoints(const Point* points, int len) ;
-Point* convexHull(Point p[], int len, unsigned int* hsize) ;
--- source/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,3 +0,0 @@
-#from DustFilaments3.FilamentPaint import *
-#from MagField import *
-#from FilPop import *
