Bug in the process of finding roots numerically has been fixed

project/DustFilaments/FilPop.py
@@ -147,7 +147,7 @@ def get_FilPop(Nfil,theta_LH_RMS,size_ratio,size_scale,slope,eta_eps,eta_fpol,Bc
             theta_LH = np.array(sorted(theta_LH,key=np.fabs))
             psi_LH_random = np.array(sorted(psi_LH_random,key=np.fabs))
             # these are the values I want to keep
-            mask_ = np.fabs(psi_LH_random) <= np.fabs(theta_LH)
+            mask_ = np.fabs(psi_LH_random) < np.fabs(theta_LH)
             psi_final.append(psi_LH_random[mask_])
             theta_final.append(theta_LH[mask_])
             N_good += np.sum(mask_)
@@ -156,22 +156,8 @@ def get_FilPop(Nfil,theta_LH_RMS,size_ratio,size_scale,slope,eta_eps,eta_fpol,Bc
         p = np.random.permutation(len(theta_LH))
         theta_LH = theta_LH[p] ; psi_LH_random = psi_LH_random[p]
         theta_LH = theta_LH[:realNfils] ; psi_LH_random = psi_LH_random[:realNfils]
-        #psi_LH_random = norm.ppf(U, loc=np.radians(0.25), scale=np.radians(10.0))
-        # we sort the theta and psi by fabs
+        #print("For fil 4461901 the angles are psi=%.8E theta=%.8E"%(psi_LH_random[4461901],theta_LH[4461901]))
         print("shape of indices of imposible angles = ",np.argwhere(np.fabs(psi_LH_random) > np.fabs(theta_LH)).shape)
-        """
-        if correct_impossible_angles:
-            print("shape of indices of imposible angles = ",np.argwhere(np.fabs(psi_LH_random) > np.fabs(theta_LH)).shape)
-            if int(0.003*realNfils) == 0:
-                Permutations(theta_LH, psi_LH_random, realNfils, 1, 1)
-            else:
-                mask_indices = np.fabs(psi_LH_random) > np.fabs(theta_LH)
-                while(mask_indices.sum()/realNfils*100.0 > 0.1):
-                    Permutations(theta_LH, psi_LH_random, realNfils, int(0.003*realNfils), 1)
-                    Permutations(theta_LH, psi_LH_random, realNfils, int(0.003*realNfils), 0)
-                    mask_indices = np.fabs(psi_LH_random) > np.fabs(theta_LH)
-                print("At the end, I have %i impossible angles"%(mask_indices.sum()))
-        """
         results = Get_Angles_Asymmetry( realNfils, Bcube, Npix_box, random_vectors, psi_LH_random, theta_LH, size, centers , theta_LH_RMS_radians)
         (angles, long_vec, psi_LH, phi_LH, phi_LH_1, phi_LH_2, thetaH, thetaL, fn_evaluated) = results
     else: